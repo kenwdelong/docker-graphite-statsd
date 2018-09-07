6a7067ccd7dac99da024ff093a0c2fc5afdb9993
Ken DeLong <kenwdelong@gmail.com>
2018-09-07 16:55:16 -0700

    updated statsd config

--- conf/opt/statsd/config_udp.js
+++ conf/opt/statsd/config_udp.js
@@ -1,9 +1,17 @@
 {
-  "graphiteHost": "127.0.0.1",
-  "graphitePort": 2003,
-  "port": 8125,
-  "flushInterval": 10000,
-  "servers": [
-    { server: "./servers/udp", address: "0.0.0.0", port: 8125 }
-  ]
+	  port: 8125,
+	  mgmt_port: 8126,
+
+	  percentThreshold: [99],
+
+	  graphitePort: 2003,
+	  graphiteHost: "127.0.0.1",
+	  flushInterval: 10000,
+	  
+	  deleteIdleStats: true,
+
+	  backends: ['./backends/graphite'],
+	  graphite: {
+	    legacyNamespace: false
+	  }
 }
